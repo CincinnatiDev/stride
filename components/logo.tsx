fc56094ab817a81c306878373e6c8db173c25ac4
CincinnatiDev <117247056+CincinnatiDev@users.noreply.github.com>
2024-01-08 20:47:46 -0500

    Initial commit

---
+++ components/logo.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
+import localFont from 'next/font/local';
+
+const headingFont = localFont({
+  src: '../public/fonts/font.woff2',
+});
+
+export const Logo = () => {
+  return (
+    <Link href='/'>
+      <div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
+        <Image src='/logo.svg' alt='logo' height={30} width={30} />
+        <p className={cn('text-lg text-neutral-700 pb-1 pt-2', headingFont.className)}>Stride</p>
+      </div>
+    </Link>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: 'Stride',
+  description: 'Collaborate, manage projects, and reach new productivity peaks.',
+};
